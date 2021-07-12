37ba91ccf754f332b32ea10366512a6229caf204
Zaidy288 <78803726+Zaidy288@users.noreply.github.com>
2021-07-12 12:59:16 +0200

    Add files via upload

---
+++ script_classes.js
@@ -0,0 +1,17 @@
+  function mySubmenuFunction() {
+    var x = document.getElementsByClassName('dropdown-content')[0];
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  function mySubmenuFunction2() {
+    var x = document.getElementsByClassName('dropdown-content2')[0];
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
